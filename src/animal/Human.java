935cef4729cf7207cdbc7c4e95d77bd8e216d230
akt-U <welderakt@gmail.com>
2023-02-05 17:37:20 +0900

    Animal継承＋Thinkable実装 private hobby

---
+++ src/animal/Human.java
@@ -0,0 +1,29 @@
+package animal;
+
+public class Human extends Animal implements Thinkable {
+    private String hobby;//趣味
+
+    public String getHobby() {
+        return hobby;
+    }
+
+    public void setHobby(String hobby) {
+        this.hobby = hobby;
+    }
+
+    public Human() {
+
+}
+    //superから引っ張り＋コンストラクタ
+    public Human(String name, int age) {
+        super(name, age);
+        this.hobby = hobby;
+    }
+
+    //thinkを実装
+    @Override
+    public void think() {
+        System.out.println("私は" + this.hobby + "について考えています");
+
+    }
+}
